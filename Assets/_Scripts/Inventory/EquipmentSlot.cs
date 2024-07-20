2d84ef602ef279c31b82f93dfb91a345c7a4a377
Niek Melet <niek+git@meletstudios.com>
2024-07-20 11:20:46 +0200

    EquipmentSlot.cs

---
+++ Assets/_Scripts/Inventory/EquipmentSlot.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace PirateJam.Inventory
+{
+    public class EquipmentSlot : ItemSlot
+    {
+        public EquipmentType EquipmentType;
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            gameObject.name = EquipmentType.ToString() + " Slot";
+        }
+    }
+}
