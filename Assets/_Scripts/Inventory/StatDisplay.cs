49422bab22120c7966269104f3b97ab7f9c0ff9f
Niek Melet <niek+git@meletstudios.com>
2024-07-20 13:35:40 +0200

    Stat display

---
+++ Assets/_Scripts/Inventory/StatDisplay.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+
+namespace PirateJam.Inventory
+{
+    public class StatDisplay : MonoBehaviour
+    {
+        public TMP_Text NameText;
+        public TMP_Text ValueText;
+
+        private void OnValidate()
+        {
+            TMP_Text[] texts = GetComponentsInChildren<TMP_Text>();
+            NameText = texts[0];
+            ValueText = texts[1];
+        }
+    }
+}
