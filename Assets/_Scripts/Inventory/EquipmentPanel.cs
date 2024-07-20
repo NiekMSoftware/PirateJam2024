06ecb703c1d42668858cad970e655327acc5042f
Niek Melet <niek+git@meletstudios.com>
2024-07-20 11:27:39 +0200

    Update EquipmentPanel
    Functionality to add and remove items from the panel

--- Assets/_Scripts/Inventory/EquipmentPanel.cs
+++ Assets/_Scripts/Inventory/EquipmentPanel.cs
@@ -13,5 +13,35 @@ namespace PirateJam.Inventory
         {
             equipmentSlots = equipmentSlotsParent.GetComponentsInChildren<EquipmentSlot>();
         }
+
+        public bool AddItem(EquipableItem item, out EquipableItem previousItem)
+        {
+            for (int i = 0; i < equipmentSlots.Length; i++)
+            {
+                if (equipmentSlots[i].EquipmentType == item.EquipmentType)
+                {
+                    previousItem = (EquipableItem)equipmentSlots[i].Item;
+                    equipmentSlots[i].Item = item;
+                    return true;
+                }
+            }
+
+            previousItem = null;
+            return false;
+        }
+
+        public bool RemoveItem(EquipableItem item)
+        {
+            for (int i = 0; i < equipmentSlots.Length; i++)
+            {
+                if (equipmentSlots[i].Item == item)
+                {
+                    equipmentSlots[i].Item = null;
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
