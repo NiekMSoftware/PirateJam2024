185e5126996cd582d8a0b4dadefd4457ca78ffd4
Niek Melet <niek+git@meletstudios.com>
2024-07-20 13:56:44 +0200

    Update CharacterStat.cs
    Calculating the final value for the stat.

--- Assets/_Scripts/CharacterStats/CharacterStat.cs
+++ Assets/_Scripts/CharacterStats/CharacterStat.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace PirateJam.CharacterStats
@@ -5,6 +6,20 @@ namespace PirateJam.CharacterStats
     public class CharacterStat
     {
         public float BaseValue;
+        public float Value {
+            get {
+                if (isDirty)
+                {
+                    _value = CalculateFinalValue();
+                    isDirty = false;
+                }
+
+                return _value;
+            }
+        }
+
+        private bool isDirty = true;
+        private float _value;
 
         private readonly List<StatModifier> statModifiers;
 
@@ -12,6 +27,31 @@ namespace PirateJam.CharacterStats
         {
             BaseValue = baseValue;
             statModifiers = new List<StatModifier>();
+        }
+
+        public void AddModifier(StatModifier modifier) 
+        { 
+            isDirty = true;
+            statModifiers.Add(modifier);
+        }
+
+        public bool RemoveModifier(StatModifier modifier)
+        {
+            isDirty = true;
+            return statModifiers.Remove(modifier);
+        }
+
+        private float CalculateFinalValue()
+        {
+            float finalValue = BaseValue;
+
+            for (int i = 0; i < statModifiers.Count; i++) 
+            {
+                finalValue += statModifiers[i].Value;
+            }
+
+            // 12.0001f != 12f
+            return (float)Math.Round(finalValue, 4);
         }
     }
 }
