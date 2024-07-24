68a659c67a144e1118b4791cef5e1388cbdb7a50
Ecks <scott.thomas.m@gmail.com>
2024-07-24 10:27:49 +0100

    Level 1 maptile and tiles update
    Created level 1 maptile and some updates to tileset images
    - Dungeontiles - added shaded areas to walls that should be darker.

--- Assets/Tiled/Tilesets/DungeonTiles.tsx
+++ Assets/Tiled/Tilesets/DungeonTiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="DungeonTiles" tilewidth="16" tileheight="16" tilecount="30" columns="6">
- <image source="../../Sprites/Environment/DungeonTiles.png" width="96" height="80"/>
+<tileset version="1.10" tiledversion="1.11.0" name="DungeonTiles" tilewidth="16" tileheight="16" tilecount="42" columns="6">
+ <image source="../../Sprites/Environment/DungeonTiles.png" width="96" height="96"/>
  <tile id="1">
   <objectgroup draworder="index" id="2">
    <object id="2" x="0" y="0" width="16" height="16"/>
@@ -56,22 +56,7 @@
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
- <tile id="12">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="13">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="26">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="27">
+ <tile id="23">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
